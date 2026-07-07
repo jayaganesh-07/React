ebbf92e4f0be6384c3b2cc5841a4917db0b28817
Jaya Ganesh <jaya07ganesh@gmail.com>
2026-07-07 17:17:41 +0530

    updated

---
+++ Day4/tailwindcomponent/src/App.jsx
@@ -0,0 +1,14 @@
+import NavBar from "./component/NavBar"
+
+
+const App = () => {
+  return (
+   <>
+   <NavBar/>
+   
+   
+   </>
+  )
+}
+
+export default App
---
+++ Day4/tailwindcomponent/src/component/NavBar.jsx
@@ -0,0 +1,27 @@
+
+
+const NavBar = () => {
+  return (
+    <>
+
+    
+    <div  className="bg-amber-200 text-amber-950 flex justify-center p-10  ">
+     <div>Logo</div>
+   
+   
+        <div  className="">
+            <a href="">Home</a>
+            <a href="">Contact</a>
+            <a href="">Login</a>
+            <a href="">Help</a>
+        </div>
+   
+    </div>
+
+    
+    
+    </>
+  )
+}
+
+export default NavBar
