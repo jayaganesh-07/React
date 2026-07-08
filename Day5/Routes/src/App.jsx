2e09f783d6a94580907704cad046572373103478
Jaya Ganesh <jaya07ganesh@gmail.com>
2026-07-08 12:02:48 +0530

    updated

---
+++ Day5/Routes/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Router, Routes } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <>
+    <Routes>
+
+
+      
+    </Routes>
+    
+    
+    </>
+  )
+}
+
+export default App
