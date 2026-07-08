34ba876b60ecf2eaaf291de3e6e1344c92b1b179
Jaya Ganesh <jaya07ganesh@gmail.com>
2026-07-08 20:26:10 +0530

    updated routes

--- Day5/Routes/src/App.jsx
+++ Day5/Routes/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-import { Router, Routes } from 'react-router-dom'
+import { Route, Router, Routes } from 'react-router-dom'
+import NavBar from './component/NavBar'
+import Home from './component/Home'
+import About from './component/About'
+import Contact from './component/Contact'
 
 const App = () => {
   return (
     <>
+    <NavBar/>
     <Routes>
+
+      <Route path="/" element={<Home/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/contact" element={<Contact/>} />
+      
 
 
       
---
+++ Day5/Routes/src/component/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const NavBar = () => {
+  return (
+    <>
+      <div className='p-3 flex justify-between items-center bg-blue-400'>
+        <div className='mx-10 text-black bg-blue-700 p-2 rounded w-20 flex justify-center items-center' >
+         Logo
+        </div>
+        <div className='mx-10 flex gap-10'>
+         <Link to="/">Home</Link>
+         <Link to="/about">About</Link>
+         <Link to="/contact">Contact</Link>
+
+        </div>
+       
+    </div>
+    </>
+  )
+}
+
+export default NavBar
