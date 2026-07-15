3988fc67b1fa7f897d017ba823da7bafe8918fe2
Jaya Ganesh <jaya07ganesh@gmail.com>
2026-07-15 22:54:50 +0530

    updated 10

---
+++ Day10/updatePhase/useState/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Counter from "./assets/component/Counter";
+import NameChange from "./assets/component/nameChange";
+import LoginStatus from "./assets/component/LoginStatus ";
+
+const App = () => {
+  return (
+    <>
+      <Counter />
+      <NameChange />
+      <LoginStatus/>
+    </>
+  );
+};
+
+export default App;
