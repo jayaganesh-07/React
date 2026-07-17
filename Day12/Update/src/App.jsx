1b6cc9b7fce5b595dd7c86e6e7ef40192820d91b
Jaya Ganesh <jaya07ganesh@gmail.com>
2026-07-17 12:02:54 +0530

    updated

---
+++ Day12/Update/src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import Array from './Component/Array'
+
+const App = () => {
+
+  const [obj,setObj]=useState({Name:"Mobile",Model:"17 Pro",Rate:10000})
+  const objChange = ()=>{
+    // const updateObj ={...obj}
+    // updateObj.Model= "18 Max"
+    //setObj(updateObj)
+    setObj({...obj,Model:"18 pro"})
+
+  }
+
+  return (
+   <>
+   <div>
+    <h1>{obj.Name}</h1>
+    <p>{obj.Model}</p>
+    <p>{obj.Rate}</p>
+    <button onClick={objChange}>Click to change</button>
+   </div>
+   <Array/>
+   </>
+  )
+}
+
+export default App
