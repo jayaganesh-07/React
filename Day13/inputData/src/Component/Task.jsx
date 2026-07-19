75baebb1a5e95a19457af30c816dc30711c9389c
Jaya Ganesh <jaya07ganesh@gmail.com>
2026-07-19 20:40:34 +0530

    updated

--- Day13/inputData/src/Component/Task.jsx
+++ Day13/inputData/src/Component/Task.jsx
@@ -25,7 +25,25 @@ const Task = () => {
         setStudentName(nameImput)
         setCourse(courseInput)
     }
-    
+    //Task-3
+    //input
+    const [employeeNameImput,setEmployeeNameInput]=useState("")
+    const [departmentInput,setDepartmentInput]=useState("")
+    const [salaryInput,setSalaryInput]=useState("")
+    //display
+    const [employeName,setEmploye]=useState("")
+    const [department,setDepartment]=useState("")
+    const [salary,setSalary]=useState("")
+
+    const handleSubmited =()=>{
+       setEmploye (employeeNameImput)
+       setDepartment(departmentInput)
+       setSalary(salaryInput)
+
+
+    }
+
+
   return (
    <>
    <div>
@@ -42,6 +60,16 @@ const Task = () => {
     <input type="text" placeholder="Student Name" value={nameImput} onChange={(e)=> setNameInput(e.target.value)} /> <br /><br />
     <input type="text" placeholder="Course " value={courseInput} onChange={(e)=>setCourseInput(e.target.value)} /> <br /><br />
     <button onClick={handleSubmit} >Click Submit</button>
+ </div>
+ <div>
+<h1>Task-3</h1>
+<p>{employeName}</p>
+<p>{department}</p>
+<p>{salary}</p>
+<input type="text" placeholder="Enter Employe Name" value={employeeNameImput} onChange={(e)=>setEmployeeNameInput(e.target.value)}/> <br /><br />
+<input type="text" placeholder="Enter the Department" value={departmentInput} onChange={(e)=>setDepartmentInput(e.target.value)} /> <br /><br />
+<input type="text" placeholder="Enter the Salary" value={salaryInput} onChange={(e)=>setSalaryInput(e.target.value)}/> <br /> <br />
+<button onClick={handleSubmited}>Click Submit</button>
  </div>
    </>
   )
