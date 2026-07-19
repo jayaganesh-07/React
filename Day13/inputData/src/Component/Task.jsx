1c5b53591e1ae09f224e545687670b5eefea69eb
Jaya Ganesh <jaya07ganesh@gmail.com>
2026-07-19 22:23:10 +0530

    updated

--- Day13/inputData/src/Component/Task.jsx
+++ Day13/inputData/src/Component/Task.jsx
@@ -15,14 +15,14 @@ const Task = () => {
 
     //Task-2
     //get input to user
-    const [nameImput,setNameInput]=useState("")
+    const [nameInput,setNameInput]=useState("")
     const [courseInput,setCourseInput]=useState("")
     //display the input
     const [studentName,setStudentName]=useState("")
     const [course,setCourse]=useState("")
 
     const handleSubmit =()=>{
-        setStudentName(nameImput)
+        setStudentName(nameInput)
         setCourse(courseInput)
     }
     //Task-3
@@ -39,9 +39,43 @@ const Task = () => {
        setEmploye (employeeNameImput)
        setDepartment(departmentInput)
        setSalary(salaryInput)
+  }
+  //Task-4
+//input
+const [productInput,setProductInput]=useState("")
+const [priceInput,setPriceInput]=useState("")
+const [categoryInput,setCategoryInput]=useState("")
+  //display
+  const [product,setProduct]=useState("")
+  const [price,setPrice]=useState("")
+  const [category,setCategory]=useState("")
+
+  const handleToSubmit =()=>{
+    setProduct(productInput)
+    setPrice(priceInput)
+    setCategory(categoryInput)
+  }
+  //Task-5
+   // Input States
+  const [nameeInput, setNameeInput] = useState("");
+  const [emailInput, setEmailInput] = useState("");
+  const [cityInput, setCityInput] = useState("");
+  const [ageInput, setAgeInput] = useState("");
+
+  // Display States
+  const [namee, setNamee] = useState("");
+  const [email, setEmail] = useState("");
+  const [city, setCity] = useState("");
+  const [age, setAge] = useState("");
+
+  const handledSubmit = () => {
+    setNamee(nameeInput);
+    setEmail(emailInput);
+    setCity(cityInput);
+    setAge(ageInput);
+  };
 
 
-    }
 
 
   return (
@@ -57,7 +91,7 @@ const Task = () => {
     <h1>Task-2</h1>
     <h1>Student Name ={studentName}</h1>
     <h1>Course ={course}</h1>
-    <input type="text" placeholder="Student Name" value={nameImput} onChange={(e)=> setNameInput(e.target.value)} /> <br /><br />
+    <input type="text" placeholder="Student Name" value={nameInput} onChange={(e)=> setNameInput(e.target.value)} /> <br /><br />
     <input type="text" placeholder="Course " value={courseInput} onChange={(e)=>setCourseInput(e.target.value)} /> <br /><br />
     <button onClick={handleSubmit} >Click Submit</button>
  </div>
@@ -71,6 +105,57 @@ const Task = () => {
 <input type="text" placeholder="Enter the Salary" value={salaryInput} onChange={(e)=>setSalaryInput(e.target.value)}/> <br /> <br />
 <button onClick={handleSubmited}>Click Submit</button>
  </div>
+
+ <div>
+     <h1>Task-4</h1>
+   <div 
+     style={{
+          border: "1px solid black",
+          width: "200px",
+          padding: "15px",
+          borderRadius: "10px",
+          backgroundColor:"blue",
+          color:"white"
+        }}>
+    
+     <p>{product}</p>
+      <p>{price}</p>
+       <p>{category}</p>
+   </div>
+       <input type="text" placeholder="Enter the Product" value={productInput}  onChange={(e)=>setProductInput(e.target.value)}/> <br /><br />
+        <input type="text" placeholder="Enter the Price" value={priceInput} onChange={(e)=>setPriceInput(e.target.value)} /> <br /><br />
+         <input type="text" placeholder="Enter the Category" value={categoryInput}  onChange={(e)=>setCategoryInput(e.target.value)}/> <br /><br />
+         <button onClick={handleToSubmit}>Click Submit</button>
+    
+ </div>
+ 
+      <h1>Task-5</h1>
+       <div
+        style={{
+          width: "280px",
+          border: "1px solid black",
+          padding: "15px",
+          borderRadius: "10px",
+        }}
+      >
+        <h2> <b>User Profile</b> </h2>
+
+        <p><b>Name:</b> {name}</p>
+        <p><b>Email:</b> {email}</p>
+        <p><b>City:</b> {city}</p>
+        <p><b>Age:</b> {age}</p>
+      </div> <br />
+  
+
+<input type="text" placeholder="Enter Name" value={nameeInput} onChange={(e) => setNameeInput(e.target.value)} /><br /><br />
+<input type="email"  placeholder="Enter Email" value={emailInput} onChange={(e) => setEmailInput(e.target.value)}/> <br /><br />
+<input  type="text" placeholder="Enter City" value={cityInput} onChange={(e) => setCityInput(e.target.value)} /> <br /><br />
+<input  type="number" placeholder="Enter Age" value={ageInput} onChange={(e) => setAgeInput(e.target.value)}  /> <br /><br />
+ <button onClick={handledSubmit}>Submit</button><br /><br />
+
+     
+  
+
    </>
   )
 }
